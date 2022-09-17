f61c9d48a68e9e4781fe50d8ce52148b3da6295c
gpriya <priyaghate@gmail.com>
2022-09-17 23:52:33 +0530

    Add files via upload

---
+++ ascii_code.c
@@ -0,0 +1,11 @@
+// Write a program which takes a character as an input and displays its ASCII code.
+#include <stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+    printf("The ascii value of %c is %d", c, c);
+
+    return 0;
+}
