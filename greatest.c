9e7e25b10bcd11e4af8fbba806f0d6373f1ffe79
gpriya <priyaghate@gmail.com>
2022-09-21 16:49:05 +0530

    Add files via upload

--- greatest.c
+++ greatest.c
@@ -6,20 +6,15 @@ int main()
     int a, b, c;
     printf("Enter 3 numbers: ");
     scanf("%d %d %d", &a, &b, &c);
-    if (a > b && a > c)
-        printf("%d is the greatest number\n", a);
-    else if (b > a && b > c)
-        printf("%d is the greatest number\n", b);
-    else if (c > a && c > b)
-        printf("%d is the greatest number\n", c);
-    else if (a == b && a > c)
-        printf("%d is the greatest number\n", a);
-    else if (a == c && a > b)
-        printf("%d is the greatest number\n", a);
-    else if (b == c && b > a)
-        printf("%d is the greatest\n", b);
+    if (a > b)
+        if (a > c)
+            printf("%d\n", a);
+        else
+            printf("%d\n", c);
     else
-        printf("%d is the greatest number\n", a);
-    
+        if (b > c)
+            printf("%d\n", b);
+        else
+            printf("%d\n", c);
     return 0;
 }
